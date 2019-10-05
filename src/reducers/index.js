746ef9ce8c3135942cdf64c823d7a2eaae9eafab
shubh <psubham94@gmail.com>
2019-10-05 10:57:50 +0530

    minor changed

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,20 +6,6 @@ const initState = {
 
 export default function productsReducer(state = initState, action) {
   switch (action.type) {
-    // case 'EDIT_PRODUCT' : {
-    //   const localProducts = state.products.map((product, index) => {
-    //     if (index === Number(action.data.id)) {
-    //       return action.data
-    //     } else {
-    //       return product
-    //     }
-    //   })
-    //   return {
-    //     ...state,
-    //     products: localProducts
-    //   }
-    // }
-
     default:
       return state;
   }
